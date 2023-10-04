892d3e2069f449c5a74d1c49e82999e4fc837ed6
nazwaikhsan <nazwa.arifah@ui.ac.id>
2023-10-04 15:54:23 +0700

    tutorial 5 selesai

--- main/views.py
+++ main/views.py
@@ -1,5 +1,6 @@
+from django.views.decorators.csrf import csrf_exempt
 import datetime
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
@@ -90,14 +91,11 @@ def logout_user(request):
     return response
 
 def edit_product(request, id):
-    # Get product berdasarkan ID
     product = Product.objects.get(pk = id)
 
-    # Set product sebagai instance dari form
     form = ProductForm(request.POST or None, instance=product)
 
     if form.is_valid() and request.method == "POST":
-        # Simpan form dan kembali ke halaman awal
         form.save()
         return HttpResponseRedirect(reverse('main:show_main'))
 
@@ -105,9 +103,27 @@ def edit_product(request, id):
     return render(request, "edit_product.html", context)
 
 def delete_product(request, id):
-    # Get data berdasarkan ID
     product = Product.objects.get(pk = id)
-    # Hapus data
     product.delete()
-    # Kembali ke halaman awal
     return HttpResponseRedirect(reverse('main:show_main'))
+
+def get_product_json(request):
+    product_item = Product.objects.all()
+    return HttpResponse(serializers.serialize('json', product_item))
+
+@csrf_exempt
+def add_product_ajax(request):
+    if request.method == 'POST':
+        name = request.POST.get("name")
+        price = request.POST.get("price")
+        description = request.POST.get("description")
+        user = request.user
+
+        new_product = Product(name=name, price=price, description=description, user=user)
+        new_product.save()
+
+        return HttpResponse(b"CREATED", status=201)
+
+    return HttpResponseNotFound()
+
+
